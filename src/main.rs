3e3c2035f5226fcdfaa28673925a17481e38689f
Harish V Chavan <addhyay@Harishs-Mac-mini.local>
2025-04-22 17:17:21 +0530

    adjusted some function calls according to borrow property requirements

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = minigrep::build(&args).unwrap_or_else(|err| {
-        println!("[ERROR] {err}");
+        eprintln!("[ERROR] {err}");
         process::exit(1);
     });
     println!("Searching for '{}' in '{}'...", config.query, config.path);
-    if let Err(e) = minigrep::run(&config) {
-        println!("[ERROR] {e}");
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("[ERROR] {e}");
         process::exit(1);
     }
 }
